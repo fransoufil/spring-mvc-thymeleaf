ecbf7c9fbac7e81cdd1ea77ac24a8b3f6f3319e4
fransoufil <fran.sf@uol.com.br>
2021-02-12 16:14:12 -0300

    Criando o projeto via wizard do STS

---
+++ src/main/java/com/franz/boot/DemoMvcT2Application.java
@@ -0,0 +1,13 @@
+package com.franz.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/franz/boot/DemoMvcT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franz.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
